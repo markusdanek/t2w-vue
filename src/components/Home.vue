e69bb0b90825f9dd5cd2f53dc0007b14048c05db
Markus Danek <mdanek@sipwise.com>
2017-03-06 16:04:39 +0100

    Init

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }} <span>asfd</span></h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    data () {
+      return {
+        msg: 'team2work'
+      }
+    }
+  }
+</script>
+
+<!-- <style lang="scss" src="../styles/layout/basic.scss"></style> -->
