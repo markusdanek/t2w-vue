8aa83ab51893355a763f9b04fb0e3cfc25623b85
Markus Danek <mdanek@sipwise.com>
2017-08-10 15:01:00 +0200

    intendation for header and hero

--- src/components/about/Hero.vue
+++ src/components/about/Hero.vue
@@ -1,50 +1,50 @@
 <template>
-  <div class="section-hero small">
-    <div class="hero-image">
-      <div class="image-src" style="background-image: url('./static/images/hero/hero-about.jpg')"></div>
-    </div>
-    <div class="container">
-      <div class="hero-text">
-        <div class="text container">
-          <h1 class="startpage">
-            team<span class="two">2</span>work für Ihren Erfolg
-          </h1>
+    <div class="section-hero small">
+        <div class="hero-image">
+            <div class="image-src" style="background-image: url('./static/images/hero/hero-about.jpg')"></div>
         </div>
-      </div>
+        <div class="container">
+            <div class="hero-text">
+                <div class="text container">
+                    <h1 class="startpage">
+                        team<span class="two">2</span>work für Ihren Erfolg
+                    </h1>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-  export default {
+export default {
     name: 'about-hero'
-  }
+}
 </script>
 
 <style scoped lang="scss">
-  @import "../../styles/util/util.scss";
+    @import "../../styles/util/util.scss";
 
-  .section-hero {
-  	@extend %hero;
-  	height: 600px;
-  	h1.startpage {
-  		@include rem(font-size, 46px);
-  		line-height: 56px;
-  		font-family: 'Open Sans';
-  		font-weight: bolder;
-  		text-transform: uppercase;
-  	}
-  	h2.startpage {
-  		@include rem(font-size, 24px);
-  		font-family: 'Open Sans';
-  		font-weight: lighter;
-  	}
-  	.hero-image {
-  		background-color: $color-black;
-  		top: -80px;
-  		.image-src {
-  			opacity: 0.6;
-  		}
-  	}
-  }
+    .section-hero {
+        @extend %hero;
+        height: 600px;
+        h1.startpage {
+            @include rem(font-size, 46px);
+            line-height: 56px;
+            font-family: 'Open Sans';
+            font-weight: bolder;
+            text-transform: uppercase;
+        }
+        h2.startpage {
+            @include rem(font-size, 24px);
+            font-family: 'Open Sans';
+            font-weight: lighter;
+        }
+        .hero-image {
+            background-color: $color-black;
+            top: -80px;
+            .image-src {
+                opacity: 0.6;
+            }
+        }
+    }
 </style>
--- src/components/app/Header.vue
+++ src/components/app/Header.vue
@@ -1,182 +1,173 @@
 <template>
-  <nav class="navbar navbar-fixed-top">
-    <div class="container">
-      <div class="col-sm-10 col-sm-offset-1">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-          <h1>
-            <router-link to="/" class="navbar-brand">team2work</router-link>
-          </h1>
+    <nav class="navbar navbar-fixed-top">
+        <div class="container">
+            <div class="col-sm-10 col-sm-offset-1">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            			<span class="sr-only">Toggle navigation</span>
+            			<span class="icon-bar"></span>
+            			<span class="icon-bar"></span>
+            			<span class="icon-bar"></span>
+    		        </button>
+                    <h1>
+                        <router-link to="/" class="navbar-brand">team2work</router-link>
+                    </h1>
+                </div>
+                <div id="navbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <router-link to="/unternehmen" class="nav-link">Für Unternehmen</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/bewerber" class="nav-link">Für Bewerber</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/ueber" class="nav-link">Über Uns</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/jobs" class="nav-link">Jobs</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/kontakt" class="nav-link">Kontakt</router-link>
+                        </li>
+                        <li v-show="authenticated">
+                            <router-link to="/backend/list" class="nav-link">Backend</router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li>
-              <router-link to="/unternehmen" class="nav-link">Für Unternehmen</router-link>
-            </li>
-            <li>
-              <router-link to="/bewerber" class="nav-link">Für Bewerber</router-link>
-            </li>
-            <li>
-              <router-link to="/ueber" class="nav-link">Über Uns</router-link>
-            </li>
-            <li>
-              <router-link to="/jobs" class="nav-link">Jobs</router-link>
-            </li>
-            <li>
-              <router-link to="/kontakt" class="nav-link">Kontakt</router-link>
-            </li>
-            <li v-show="authenticated">
-              <router-link to="/backend/list" class="nav-link">Backend</router-link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </nav>
+    </nav>
 </template>
 
 <script>
-  export default {
-    name: 'header',
-    mounted() {
-      $(window).scroll(function () {
-        if ($(window).scrollTop() > 80) {
-          $('nav.navbar').addClass('scroll');
-          $('h1 a.navbar-brand').addClass('shrink');
+    export default {
+        name: 'header',
+        mounted() {
+            $(window).scroll(function() {
+                if ($(window).scrollTop() > 80) {
+                    $('nav.navbar').addClass('scroll');
+                    $('h1 a.navbar-brand').addClass('shrink');
+                } else {
+                    $('nav.navbar').removeClass('scroll');
+                    $('h1 a.navbar-brand').removeClass('shrink');
+                }
+            });
+        },
+        created() {
+            this.checkAuth();
+        },
+        methods: {
+            checkAuth() {
+                if (localStorage.getItem('profile')) {
+                    this.authenticated = true;
+                } else {
+                    this.authenticated = false;
+                }
+            }
         }
-        else {
-          $('nav.navbar').removeClass('scroll');
-          $('h1 a.navbar-brand').removeClass('shrink');
-        }
-      });
-    },
-    created() {
-      this.checkAuth();
-    },
-    methods: {
-      checkAuth() {
-        if (localStorage.getItem('profile')) {
-          this.authenticated = true;
-        } else {
-          this.authenticated = false;
-        }
-      }
     }
-  }
 </script>
 
 <style scoped lang="scss">
-  @import "../../styles/util/util.scss";
+    @import "../../styles/util/util.scss";
 
-  nav.navbar {
-  	@include transition(all 0.5s ease);
-  	@include rem((margin: 0));
-  	background: transparent;
-  	border-right: 10px solid $color-red-t2w;
-  }
-  nav.navbar.scroll {
-  	@include transition(all 0.5s ease);
-  	background: $color-white;
-  	box-shadow: 0 1px 3px rgba(0,0,0,.1);
-  	.nav > li > a {
-  		color: $color-black-medium;
-  		&:hover {
-  			text-decoration: underline;
-  		}
-  	}
-  	h1 a.navbar-brand {
-  		background: url("/static/images/logo/team2work.svg") no-repeat;
-  		font-family: 'Open Sans';
-  	}
-  }
-  h1 a.navbar-brand {
-  	@include transition(all 0.5s ease);
-  	text-indent: -999px;
-  	background: url("/static/images/logo/team2work-white.svg") no-repeat;
-  	display: block;
-  	width: 220px;
-  	height: 130px;
-  	font-family: 'Open Sans';
+    nav.navbar {
+        @include transition(all 0.5s ease);
+        @include rem((margin: 0));
+        background: transparent;
+        border-right: 10px solid $color-red-t2w;
+    }
+    nav.navbar.scroll {
+        @include transition(all 0.5s ease);
+        background: $color-white;
+        box-shadow: 0 1px 3px rgba(0,0,0,.1);
+        .nav > li > a {
+            color: $color-black-medium;
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+        h1 a.navbar-brand {
+            background: url("/static/images/logo/team2work.svg") no-repeat;
+            font-family: 'Open Sans';
+        }
+    }
+    h1 a.navbar-brand {
+        @include transition(all 0.5s ease);
+        text-indent: -999px;
+        background: url("/static/images/logo/team2work-white.svg") no-repeat;
+        display: block;
+        width: 220px;
+        height: 130px;
+        font-family: 'Open Sans';
 
-  	&.shrink {
-  		@include transition(all 0.5s ease);
-  		width: 130px;
-  		height: 80px;
-  	}
-  }
-  @media (max-width: $screen-md) {
-  	h1 a.navbar-brand {
-  		width: 130px;
-  		height: 80px;
-  	}
-  }
-  .navbar-right {
-  	@include rem((margin: 15px 15px 0 0));
-  }
-  @media only screen
-  and (min-device-width: 768px)
-  and (max-device-width: 1024px)
-  and (orientation: portrait)
-  and (-webkit-min-device-pixel-ratio: 1) {
-  	.navbar-right {
-  		@include rem((margin: -65px -50px 0 0));
-  	}
-  }
-  @media only screen
-  and (min-device-width: 768px)
-  and (max-device-width: 1024px)
-  and (orientation: portrait)
-  and (-webkit-min-device-pixel-ratio: 2) {
-  	.navbar-right {
-  		@include rem((margin: -65px -50px 0 0));
-  	}
-  }
-  .nav > li > a {
-  	color: $color-white;
-  	&:focus,
-  	&:hover {
-  		@include transition(all .3s ease);
-  		background-color: transparent;
-  		text-decoration: none;
-  		color: $color-red-t2w;
-  		border-bottom: none;
-  	}
-  }
-  .navbar-toggle {
-  	@include rem((margin-top: 27px));
-  	.icon-bar {
-  		background-color: $color-red-t2w;
-  	}
-  }
-  .navbar-collapse.in {
-  	background: $color-white;
-  	@include rem((margin: 0 -15px));
-  	@include rem((padding: 0 30px));
-  	box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
-  	.nav > li > a {
-  		color: $color-black-medium;
-  	}
-  }
-  @media (max-device-width: 480px) and (orientation: landscape) {
-  	.navbar-fixed-bottom .navbar-collapse,
-  	.navbar-fixed-top .navbar-collapse {
-  		max-height: 220px;
-  	}
-  }
-  @media (min-width: 768px) and (max-width: 991px) {
-  	.navbar-right {
-  		@include rem((margin: 0 ));
-  		position: relative;
-  		left: 70px;
-  		top: -65px;
-  	}
-  }
-  .navbar-collapse.in .nav > li > a:hover {
-  	text-decoration: underline;
-  }
+        &.shrink {
+            @include transition(all 0.5s ease);
+            width: 130px;
+            height: 80px;
+        }
+    }
+    @media (max-width: $screen-md) {
+        h1 a.navbar-brand {
+            width: 130px;
+            height: 80px;
+        }
+    }
+    .navbar-right {
+        @include rem((margin: 15px 15px 0 0));
+    }
+    @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (orientation: portrait) and (-webkit-min-device-pixel-ratio: 1) {
+        .navbar-right {
+            @include rem((margin: -65px -50px 0 0));
+        }
+    }
+    @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (orientation: portrait) and (-webkit-min-device-pixel-ratio: 2) {
+        .navbar-right {
+            @include rem((margin: -65px -50px 0 0));
+        }
+    }
+    .nav > li > a {
+        color: $color-white;
+        &:focus,
+        &:hover {
+            @include transition(all .3s ease);
+            background-color: transparent;
+            text-decoration: none;
+            color: $color-red-t2w;
+            border-bottom: none;
+        }
+    }
+    .navbar-toggle {
+        @include rem((margin-top: 27px));
+        .icon-bar {
+            background-color: $color-red-t2w;
+        }
+    }
+    .navbar-collapse.in {
+        background: $color-white;
+        @include rem((margin: 0 -15px));
+        @include rem((padding: 0 30px));
+        box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+        .nav > li > a {
+            color: $color-black-medium;
+        }
+    }
+    @media (max-device-width: 480px) and (orientation: landscape) {
+        .navbar-fixed-bottom .navbar-collapse,
+        .navbar-fixed-top .navbar-collapse {
+            max-height: 220px;
+        }
+    }
+    @media (min-width: 768px) and (max-width: 991px) {
+        .navbar-right {
+            @include rem((margin: 0 ));
+            position: relative;
+            left: 70px;
+            top: -65px;
+        }
+    }
+    .navbar-collapse.in .nav > li > a:hover {
+        text-decoration: underline;
+    }
 </style>
