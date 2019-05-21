2a060352fef3c6bd3f2f39908e324183578f0996
Markus Danek <Markus.Danek-CIC@ibm.com>
2019-05-21 10:05:47 +0200

    Deprecate 'karriere' and use 'standard' instead for new jobs

--- src/views/backend/JobAdd.vue
+++ src/views/backend/JobAdd.vue
@@ -25,13 +25,13 @@
               <label for="salary" class="control-label">Gehalt</label>
               <b-col>
                 <input type='text' name='salary' v-model="job.salary" class='form-control'>
-                </b-col>
+              </b-col>
             </div>
             <div class="form-group">
               <label for="hours" class="control-label">Wochenstunden</label>
               <b-col>
                 <input type='text' name='hours' v-model="job.hours" class='form-control' required>
-                  </b-col>
+              </b-col>
             </div>
             <hr>
             <h2>Optional</h2>
@@ -40,83 +40,83 @@
               <b-col>
                 <div class="checkbox">
                   <input type="checkbox" @change="onChange('stepstone', $event)"> Stepstone
-                                </div>
-                  <div class="checkbox">
-                    <input type="checkbox" @change="onChange('karriere', $event)"> Karriere
-                                </div>
+                </div>
+                <div class="checkbox">
+                  <input type="checkbox" @change="onChange('standard', $event)"> Der Standard
+                </div>
               </b-col>
             </div>
             <div class="form-group">
               <label for="referenceId" class="col-sm-3 control-label">Referenznummer</label>
               <b-col>
                 <input type='text' name='referenceId' v-model="job.referenceId" class='form-control'>
-                        </b-col>
+              </b-col>
             </div>
             <div class="form-group">
               <label for="title" class="col-sm-3 control-label">Zweiter Titel</label>
               <b-col>
                 <input type='text' name='title' v-model="job.subTitle" class='form-control'>
-                          </b-col>
+              </b-col>
             </div>
             <div class="form-group">
               <label for="subText" class="col-sm-3 control-label">Job Untertitel (nach Titel der Anzeige)</label>
               <b-col>
                 <input type='text' name='subText' v-model="job.subText" class='form-control'>
-                            </b-col>
+              </b-col>
             </div>
             <div class="form-group">
               <label for="minSalary" class="col-sm-3 control-label">Min. Gehalt</label>
               <b-col>
                 <input type='text' name='minSalary' v-model="job.minSalary" class='form-control'>
-                            </b-col>
+              </b-col>
             </div>
             <div class="form-group">
               <label for="maxSalary" class="col-sm-3 control-label">Max. Gehalt</label>
               <b-col>
                 <input type='text' name='maxSalary' v-model="job.maxSalary" class='form-control'>
-                            </b-col>
+              </b-col>
             </div>
             <div class="form-group">
               <label for="salaryText" class="col-sm-3 control-label">Zusatztext f&uuml;r Gehalt</label>
               <b-col>
                 <input type='text' name='salaryText' v-model="job.salaryText" class='form-control'>
-                            </b-col>
+              </b-col>
             </div>
             <div class="form-group">
               <label for="area" class="col-sm-3 control-label">Gebiet</label>
               <b-col>
                 <input type='text' name='area' v-model="job.area" class='form-control'>
-                            </b-col>
+              </b-col>
             </div>
             <div class="form-group">
               <label for="introText" class="col-sm-3 control-label">Einleitungstext</label>
               <b-col>
                 <input type='text' name='introText' v-model="job.introText" class='form-control'>
-                            </b-col>
+              </b-col>
             </div>
             <div class="form-group">
               <label for="moreInfoText" class="col-sm-3 control-label">Unser Kunde bietet</label>
               <b-col>
                 <input type='text' name='moreInfoText' v-model="job.moreInfoText" class='form-control'>
-                            </b-col>
+              </b-col>
             </div>
             <div class="form-group">
               <label for="email" class="col-sm-3 control-label">E-Mail</label>
               <b-col>
                 <input type='email' name='email' v-model="job.email" class='form-control'>
-                            </b-col>
+              </b-col>
             </div>
             <div class="form-group">
               <label for="contact" class="col-sm-3 control-label">Ansprechperson</label>
               <b-col>
                 <input type='text' name='contact' v-model="job.contact" class='form-control'>
-                            </b-col>
+              </b-col>
             </div>
             <div class="form-group">
               <label for="expectText" class="col-sm-3 control-label">Erwartungstext</label>
               <b-col>
                 <input type='text' name='expectText' v-model="job.expectText" class='form-control'>
-                            </b-col>
+              </b-col>
             </div>
             <hr>
             <h2>Qualifikationen</h2>
@@ -124,12 +124,12 @@
               <label for="qualifications" class="col-sm-3 control-label">Qualifikation</label>
               <b-col>
                 <input type='text' name='qualifications' v-model="qual.text" class='form-control'>
-                            </b-col>
+              </b-col>
             </div>
             <b-row>
               <a @click.prevent="addQualification" class="col-sm-offset-3 addmore">
-                                + Neue Qualifikation
-                            </a>
+                + Neue Qualifikation
+              </a>
             </b-row>
             <hr>
             <h2>Aufgaben</h2>
@@ -137,19 +137,19 @@
               <label for="responsibility" class="col-sm-3 control-label">Aufgaben</label>
               <b-col>
                 <input type='text' name='responsibility' v-model="resp.text" class='form-control'>
-                            </b-col>
+              </b-col>
             </div>
             <b-row>
               <a @click.prevent="addResponsibility" class="col-sm-offset-3 addmore">
-                                + Neue Verantwortung
-                            </a>
+                + Neue Verantwortung
+              </a>
             </b-row>
             <hr>
             <b-row>
               <b-col>
                 <button class='submit-btn btn' type='submit' style="display: block;margin: 10px 0 20px 0;">
-                                    Job hinzufügen
-                                </button>
+                  Job hinzufügen
+                </button>
               </b-col>
             </b-row>
           </form>
@@ -158,11 +158,11 @@
           <div class="alert alert-info" role="alert">
             <b-row>
               <b-col>
-                <b>Fett</b>: __Wort__ (zwei Unterstrich vor und nach dem Wort)<br/>
+                <b>Fett</b>: __Wort__ (zwei Unterstrich vor und nach dem Wort)<br />
               </b-col>
-                <b-col>
-                  <em>Kursiv</em>: _Wort_ (ein Unterstrich vor und nach dem Wort)
-                </b-col>
+              <b-col>
+                <em>Kursiv</em>: _Wort_ (ein Unterstrich vor und nach dem Wort)
+              </b-col>
             </b-row>
           </div>
         </div>
@@ -266,8 +266,7 @@ export default {
   metaInfo: {
     title: 'team2work',
     titleTemplate: '%s » Job hinzufügen',
-    meta: [
-      {
+    meta: [{
         'property': 'og:title',
         'content': 'team2work',
         'template': chunk => `${chunk} - Job hinzufügen`,
